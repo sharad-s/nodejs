a10edb0cee7d41032c4f085b5597578aac903515
Sharad <sharadshekar@gmail.com>
2024-07-06 18:05:39 -0500

    yh

--- index.js
+++ index.js
@@ -1,12 +1,75 @@
+const express = require('express');
 const http = require('http');
-const PORT = 3000;
+const { Server } = require('socket.io');
+const cors = require('cors');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const app = express();
+app.use(cors()); // Enable CORS
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST"]
+  }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+let clients = {};
+let callClients = {};
+
+io.on('connection', (socket) => {
+  console.log(`Client connected: ${socket.id}`);
+  clients[socket.id] = socket.id;
+  io.emit('clients', Object.keys(clients));
+  io.emit('callClients', Object.keys(callClients));
+  console.log(`Current clients: ${Object.keys(clients)}`);
+
+  socket.on('joinCall', () => {
+    console.log(`Client joined call: ${socket.id}`);
+    callClients[socket.id] = socket.id;
+    io.emit('callClients', Object.keys(callClients));
+  });
+
+  socket.on('leaveCall', () => {
+    console.log(`Client left call: ${socket.id}`);
+    delete callClients[socket.id];
+    io.emit('callClients', Object.keys(callClients));
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Client disconnected: ${socket.id}`);
+    delete clients[socket.id];
+    delete callClients[socket.id];
+    io.emit('clients', Object.keys(clients));
+    io.emit('callClients', Object.keys(callClients));
+    console.log(`Current clients: ${Object.keys(clients)}`);
+  });
+
+  socket.on('signal', (data) => {
+    console.log(`Signal received from ${socket.id} to ${data.to}`);
+    io.to(data.to).emit('signal', { from: socket.id, signal: data.signal });
+  });
+
+  socket.on('reset', () => {
+    console.log('Resetting all connections');
+    io.emit('forceDisconnect');
+  });
+});
+
+app.post('/reset', (req, res) => {
+  console.log('Reset endpoint called');
+  io.emit('forceDisconnect');
+  clients = {};
+  callClients = {};
+  io.emit('clients', Object.keys(clients));
+  io.emit('callClients', Object.keys(callClients));
+  res.send('All connections have been reset');
+});
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+server.listen(3000, () => {
+  console.log('Signaling server running on port 3000');
 });
